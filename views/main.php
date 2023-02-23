c213f1bb11dcdaa2a7ebbcae4261b6023263e0fd
Gavin Henry <ghenry@sentrypeer.org>
2023-02-23 18:17:35 +0000

    Add our logo.

--- views/main.php
+++ views/main.php
@@ -10,8 +10,27 @@
 <!--                             |___/                      -->
 
 <div class="container-fluid">
-    <h1><?php echo _("SentryPeer") ?></h1>
-    <h2><?php echo $subhead ?></h2>
+         <svg viewBox="0 0 40.302814 53.481709" width="48" height="48" class="rounded float-left img-thumbnail" aria-hidden="true">
+              <g id="layer1" transform="translate(-53.112717,-43.907589)">
+                <g id="g12" transform="matrix(0.35277777,0,0,-0.35277777,73.26413,43.907589)" style="fill:#ffffff;fill-opacity:1">
+                  <path d="M 0,0 H -17.647 L 0,-70.805 Z" style="fill:#6d50a0;fill-opacity:1;fill-rule:nonzero;stroke:none"
+id="path14"></path>
+                </g>
+                <g id="g16" transform="matrix(0.35277777,0,0,-0.35277777,65.564008,47.738649)" style="fill:#6d50a0;fill-opacity:1">
+                  <path d="m 0,0 21.827,-79.059 v -40.398 l -11.946,53.864 -26.6,33.448 v -39.963 l 26.472,-21.285 3.25,-47.349
+-48.298,38.661 v 86.009 z" style="fill:#6d50a0;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path18"></path>
+                </g>
+                <g id="g20" transform="matrix(0.35277777,0,0,-0.35277777,73.26413,43.907589)" style="fill:#6d50a0;fill-opacity:1">
+                  <path d="M 0,0 H 17.647 L 0,-70.805 Z" style="fill:#6d50a0;fill-opacity:1;fill-rule:nonzero;stroke:none"
+id="path22"></path>
+                </g>
+                <g id="g24" transform="matrix(0.35277777,0,0,-0.35277777,80.96424,47.738649)" style="fill:#6d50a0;fill-opacity:1">
+                  <path d="m 0,0 -21.827,-79.059 v -40.398 l 11.946,53.864 26.6,33.448 v -39.963 l -26.472,-21.285 -3.25,-47.349
+48.298,38.661 v 86.009 z" style="fill:#6d50a0;fill-opacity:1;fill-rule:nonzero;stroke:none" id="path26"></path>
+                </g>
+              </g>
+            </svg>
+    <p class="lead"><?php echo $subhead ?></p>
     <div class="display full-border">
         <div class="row">
             <div class="col-sm-12">
